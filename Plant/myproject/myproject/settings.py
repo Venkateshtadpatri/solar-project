fa730539474d7b1a8b88e5eff5b8a83816f933c8
saigopalbonthu-WebDev <saigopalbonthu@gmail.com>
2024-12-12 17:06:57 +0530

    Update settings.py

--- Plant/myproject/myproject/settings.py
+++ Plant/myproject/myproject/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  # Your React app's URL
+    "https://solar-layout-generator.vercel.app",  # Your React app's URL
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_METHODS = [
