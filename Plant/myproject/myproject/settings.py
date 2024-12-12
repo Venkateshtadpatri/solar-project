76b277de22e87aa80e4f1bacacc2f65ce327e6c7
saigopalbonthu-WebDev <saigopalbonthu@gmail.com>
2024-12-12 17:11:40 +0530

    Update settings.py

--- Plant/myproject/myproject/settings.py
+++ Plant/myproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^6oapujg8t#vzj5nm_q$f+pou^2e4==%k+(_j(f+hbr789o7vr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
